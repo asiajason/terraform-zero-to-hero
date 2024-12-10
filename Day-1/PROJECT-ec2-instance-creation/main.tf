36201a503e02a761b06fb2360c9802d740abe0da
Jason <jasonfuller2000@gmail.com>
2024-12-10 02:22:44 +0000

    changed desc to 3

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "example" {
     ami           = "ami-047126e50991d067b"  # Specify an appropriate AMI ID Windows 2025 Base
     instance_type = "t3.large"
     tags = { 
-        Name = "Bastion-Testing2" 
+        Name = "Bastion-Testing3" 
         Environment = "Development" 
         Project = "DevProject"
     }
